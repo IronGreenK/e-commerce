c91bc6f5a62e3ab47898f8de7106f3a12089b506
Lint Action <lint-action@samuelmeuli.com>
2022-10-15 00:49:55 +0000

    Fix code style issues with ESLint

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -1,121 +1,117 @@
 const axios = require('axios')
-const {faker} = require('@faker-js/faker')
-const {server} = require('../../src/network')
+const { faker } = require('@faker-js/faker')
+const { server } = require('../../src/network')
 
 const URL = `http://localhost:${process.env.PORT || 3000}`
 
 beforeAll(async () => {
-    await server.start()
+  await server.start()
 })
 
 afterAll(async () => {
-    await server.stop()
+  await server.stop()
 })
 
 describe('API: GET /', () => {
-    let response = {}
+  let response = {}
 
-    test('Should return 200 as status code', async () => {
-        response = await axios.get(URL)
-        expect(response.status).toBe(200)
-    })
+  test('Should return 200 as status code', async () => {
+    response = await axios.get(URL)
+    expect(response.status).toBe(200)
+  })
 
-    test('Should be a successful operation', () => {
-        expect(response.data.error).toBe(false)
-    })
+  test('Should be a successful operation', () => {
+    expect(response.data.error).toBe(false)
+  })
 })
 
 describe('E2E test: Use cases from UserService', () => {
+  // User Mode Client
+  const name = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const userName = faker.internet.userName()
+  const balance = faker.random.numeric()
+  const newUser = {
+    name,
+    lastName,
+    userName,
+    balance,
+    email: faker.internet.email(name, lastName).toLowerCase(),
+    password: faker.datatype.string()
+  }
+  const tokens = {
+    accessToken: '',
+    refreshToken: ''
+  }
 
-    // User Mode Client
-    const name = faker.name.firstName()
-    const lastName = faker.name.lastName()
-    const userName = faker.internet.userName()
-    const balance = faker.random.numeric()
-    const newUser = {
-        name,
-        lastName,
-        userName,
-        balance,
-        email: faker.internet.email(name, lastName).toLowerCase(),
-        password: faker.datatype.string()
-    }
-    const tokens = {
-        accessToken: '',
-        refreshToken: ''
-    }
+  // User Mode Vendor
+  const name2 = faker.name.firstName()
+  const lastName2 = faker.name.lastName()
+  const newUser2 = {
+    name: name2,
+    lastName: lastName2,
+    userName: faker.internet.userName(),
+    balance: 300000,
+    email: faker.internet.email(name2, lastName2).toLowerCase(),
+    password: faker.datatype.string(),
+    role: '1'
+  }
+  const tokens2 = {
+    accessToken: '',
+    refreshToken: ''
+  }
 
+  describe('Testing save user as client', () => {
+    let response = {}
 
-    // User Mode Vendor
-    const name2 = faker.name.firstName()
-    const lastName2 = faker.name.lastName()
-    const newUser2 = {
-        name: name2,
-        lastName: lastName2,
-        userName: faker.internet.userName(),
-        balance: 300000,
-        email: faker.internet.email(name2, lastName2).toLowerCase(),
-        password: faker.datatype.string(),
-        role: '1'
-    }
-    const tokens2 = {
-        accessToken: '',
-        refreshToken: ''
-    }
-
-    describe('Testing save user as client', () => {
-        let response = {}
-
-        test('Should return 201 as status code', async () => {
-            response = await axios.post(`${URL}/api/user/signup`, newUser)
-            expect(response.status).toBe(201)
-        })
+    test('Should return 201 as status code', async () => {
+      response = await axios.post(`${URL}/api/user/signup`, newUser)
+      expect(response.status).toBe(201)
     })
+  })
 
-    describe('Testing login a user as client', () => {
-        const keys = ['accessToken', 'refreshToken']
+  describe('Testing login a user as client', () => {
+    const keys = ['accessToken', 'refreshToken']
 
-        test('Should return accessToken and refreshToken', async () => {
-            const {
-                data: {message}
-            } = await axios.post(`${URL}/api/user/login`, {
-                email: newUser.email,
-                password: newUser.password
-            })
+    test('Should return accessToken and refreshToken', async () => {
+      const {
+        data: { message }
+      } = await axios.post(`${URL}/api/user/login`, {
+        email: newUser.email,
+        password: newUser.password
+      })
 
-            expect(Object.keys(message)).toEqual(keys)
-            tokens.accessToken = message.accessToken
-            tokens.refreshToken = message.refreshToken
-        })
+      expect(Object.keys(message)).toEqual(keys)
+      tokens.accessToken = message.accessToken
+      tokens.refreshToken = message.refreshToken
     })
+  })
 
+  describe('Testing save user as vendor', () => {
+    let response = {}
 
-    describe('Testing save user as vendor', () => {
-        let response = {}
-
-        test('Should return 201 as status code', async () => {
-            response = await axios.post(`${URL}/api/user/signup`, newUser2)
-            expect(response.status).toBe(201)
-        })
+    test('Should return 201 as status code', async () => {
+      response = await axios.post(`${URL}/api/user/signup`, newUser2)
+      expect(response.status).toBe(201)
     })
+  })
 
-    describe('Testing login a user as vendor', () => {
-        const keys = ['accessToken', 'refreshToken']
+  describe('Testing login a user as vendor', () => {
+    const keys = ['accessToken', 'refreshToken']
 
-        test('Should return accessToken and refreshToken', async () => {
-            const {
-                data: {message}
-            } = await axios.post(`${URL}/api/user/login`, {
-                email: newUser2.email,
-                password: newUser2.password
-            })
+    test('Should return accessToken and refreshToken', async () => {
+      const {
+        data: { message }
+      } = await axios.post(`${URL}/api/user/login`, {
+        email: newUser2.email,
+        password: newUser2.password
+      })
 
-            expect(Object.keys(message)).toEqual(keys)
-            tokens2.accessToken = message.accessToken
-            tokens2.refreshToken = message.refreshToken
-        })
+      expect(Object.keys(message)).toEqual(keys)
+      tokens2.accessToken = message.accessToken
+      tokens2.refreshToken = message.refreshToken
     })
-
+  })
 })
 
 /**
